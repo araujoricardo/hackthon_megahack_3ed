c69bd22055636ee8c155d2cb4cb8c8310c6e8f5f
araujoricardo <r_araujo_pereira@hotmail.com>
2020-07-04 12:00:29 -0300

    inicio lógica quiz

---
+++ frontend/src/components/CardType1/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function CardType1(props){
+    return(
+        <div>
+            <p>{props.text}</p>
+        </div>
+    );
+};
---
+++ frontend/src/contents/content1.js
@@ -0,0 +1,35 @@
+export const content1 = [
+    {
+        type: "card1",
+        text: "texto legal aqui",
+        options: {
+            a: "primeira opçao",
+            b: "segunda opcao",
+            c: "terceira opcao",
+            d: "quarta opcao"
+        },
+        answer : "b"
+    },
+    {
+        type: "card1",
+        text: "texto legal aqui",
+        options: {
+            a: "primeira opçao",
+            b: "segunda opcao",
+            c: "terceira opcao",
+            d: "quarta opcao"
+        },
+        answer : "b"
+    },
+    {
+        type: "card1",
+        text: "texto legal aqui",
+        options: {
+            a: "primeira opçao",
+            b: "segunda opcao",
+            c: "terceira opcao",
+            d: "quarta opcao"
+        },
+        answer : "b"
+    }
+];
