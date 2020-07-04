df11ad2c50c6f4db37d9d6a157a0bcfdbdc6a426
araujoricardo <r_araujo_pereira@hotmail.com>
2020-07-04 16:56:03 -0300

    sistema de respostas

--- frontend/src/components/CardType1/index.js
+++ frontend/src/components/CardType1/index.js
@@ -1,10 +1,39 @@
 import React from "react";
-
+import {Card1Wrapper} from "./style";
 
 export default function CardType1(props){
+
+    const question = props.element;
+    
+    const handleOnChange=(event)=>{
+        props.handleAnswer(event.target.value);
+    };
+    
     return(
-        <div>
-            <p>{props.text}</p>
-        </div>
+        <Card1Wrapper>
+            <p>question</p>
+            <p>{question.text}</p>
+            <p>Resposta: </p>
+
+            <input type="checkbox" 
+                onChange={handleOnChange}
+                value="a"/>
+            <label>{question.options.a}</label>
+            <br/>
+            <input type="checkbox" 
+                onChange={handleOnChange}
+                value="b"/>
+            <label>{question.options.b}</label>
+            <br/>
+            <input type="checkbox" 
+                onChange={handleOnChange}
+                value="c"/>
+            <label>{question.options.c}</label>
+            <br/>
+            <input type="checkbox" 
+                onChange={handleOnChange}
+                value="d"/>
+            <label>{question.options.d}</label>
+        </Card1Wrapper>
     );
 };
--- frontend/src/pages/Quiz/index.js
+++ frontend/src/pages/Quiz/index.js
@@ -2,32 +2,63 @@ import React from "react";
 import CardType1 from "../../components/CardType1";
 import {content1} from "../../contents/content1";
 import { connect } from "react-redux";
+import {setQuestion, setAnswer} from "../../containers/actions/quiz";
 
 
 class Quiz extends React.Component{
 
     renderContent = () =>{
-        // return content1.map((element)=>{
-        //     return (
-        //         <CardType1
-        //             text={element.text}
-        //         />
-        //     );
-        // });
+        const question = content1[this.props.currentQuestion];
 
-        const element = content1[this.props.content1];
-        return (
-            <CardType1
-                text={content1[0].text}
-            />
-        );
+        switch(question.type){
+            case "card1":{
+                return (<CardType1 element={question} 
+                                handleAnswer={this.handleAnswer}/>)
+            };
+            case "card2":{
+                return console.log("card2");
+            };
+        };
     };
+
+
+    handleAnswer = (value) =>{
+        const {answer, currentQuestion, setAnswer} = this.props;
+
+        const answers = answer;
+        answers[currentQuestion] = value;
+
+        setAnswer(answers);
+    };
+
+    
+    handleChangeQuestion = (value) =>{
+        const {currentQuestion, answer} = this.props
+        const aux = currentQuestion + value;
+
+
+        if(content1[currentQuestion].answer === answer[currentQuestion]){
+            console.log("resposta certa");
+        }else{
+            console.log("resposta errada");
+        };
+
+        if(content1[aux]){
+            this.props.setQuestion(value);
+        };
+    };
+
+
+
+
 
     render(){
         return(
             <div>
                 QUIZ
                 {this.renderContent()}
+                <button onClick={()=>this.handleChangeQuestion(1)}>next</button>
+                <button onClick={()=>this.handleChangeQuestion(-1)}>back</button>
             </div>
         );
     };
@@ -35,9 +66,16 @@ class Quiz extends React.Component{
 };
 
 const mapStateToProps = (state) => ({
-    currentQuestion: state.quiz.currentQuestion
+    currentQuestion: state.quiz.currentQuestion,
+    answer : state.quiz.answer
 });
 
+const mapDispatchToProps = (dispatch) => {
+    return{
+        setQuestion: (value) => dispatch(setQuestion(value)),
+        setAnswer: (value) => dispatch(setAnswer(value))
+    };
+};
 
 
-export default connect(mapStateToProps, null) (Quiz);
+export default connect(mapStateToProps, mapDispatchToProps) (Quiz);
